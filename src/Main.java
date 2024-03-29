662526617a2514585743000f65683a9379ac5401
Henry Betancourth <69738617+noelys215@users.noreply.github.com>
2024-03-29 19:18:11 -0400

    started JFrame templates

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,15 @@
+import javax.swing.*;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        int boardWidth = 600;
+        int boardHeight = 600;
+
+        JFrame frame = new JFrame("Snake");
+        frame.setVisible(true);
+        frame.setSize(boardWidth, boardHeight);
+        frame.setLocationRelativeTo(null);
+        frame.setResizable(false);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
     }
 }
